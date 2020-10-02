1c528b2e2198b82d413f685d56c084ad69861595
new-thanh <new-thanh@eplus.vn>
2020-10-02 20:26:06 +0700

    reset

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
-// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import { render } from 'react-snapshot';
 
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
@@ -13,17 +12,16 @@ const options = {
   // you can also just use 'bottom center'
   position: positions.BOTTOM_RIGHT,
   timeout: 5000,
-  offset: "30px",
+  offset: '30px',
   // you can also just use 'scale'
-  transition: transitions.SCALE,
-};
+  transition: transitions.SCALE
+}
 
-// ReactDOM.render(
-  render(
-  <AlertProvider template={AlertTemplate} {...options}>
+ReactDOM.render(
+    <AlertProvider template={AlertTemplate} {...options}>
     <App />
-  </AlertProvider>,
-  document.getElementById("root")
+    </AlertProvider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
